d80434aedf90af1ffa06770e71ed2c5ca3cd52e7
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2022-09-07 23:36:18 -0300

    feat: fazendo validação com o toast, na criação de tarefas.

--- client/GerenciadorTarefa/src/app/tarefa/criar/tarefa-criar.component.ts
+++ client/GerenciadorTarefa/src/app/tarefa/criar/tarefa-criar.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IHttpTarefaService } from 'src/app/shared/interfaces/ihttp-tarefa-service';
+import { ToastService } from 'src/app/shared/services/toast.service';
 import { TarefaCreateViewModel } from 'src/app/shared/viewModels/tarefa/tarefa-create-view-model';
 
 @Component({
@@ -13,7 +14,7 @@ export class TarefaCriarComponent implements OnInit {
   cadastroForm: FormGroup;
   tarefa: TarefaCreateViewModel;
 
-  constructor(@Inject('IHttpTarefaServiceToken') private servicoTarefa: IHttpTarefaService, private router: Router) { }
+  constructor(@Inject('IHttpTarefaServiceToken') private servicoTarefa: IHttpTarefaService, private router: Router, private toastService: ToastService) { }
 
   ngOnInit(): void {
     this.cadastroForm = new FormGroup({
@@ -23,27 +24,25 @@ export class TarefaCriarComponent implements OnInit {
   }
 
   adicionarTarefa() {
-    
-
       this.tarefa = Object.assign({}, this.tarefa, this.cadastroForm.value);
       console.log('antes de entrar no servico')
       this.servicoTarefa.adicionarTarefas(this.tarefa)
       .subscribe(
         tarefa => {
-          /*this.toastService.show('Funcionario ' + funcionario.nome + ' adicionado com sucesso!',
-            { classname: 'bg-success text-light', delay: 5000 });*/
-            console.log('Tarefa adicionada com sucesso', tarefa.titulo, tarefa.descricao)
+          this.toastService.show('Tarefa ' + tarefa.titulo + ' adicionada com sucesso!',
+            { classname: 'bg-success text-light', delay: 5000 });
+            //console.log('Tarefa adicionada com sucesso', tarefa.titulo, tarefa.descricao)
           setTimeout(() => {
             this.router.navigate(['tarefa/listar']);
           }, 5000);
         },
         erro => {
+          let mensagemErro = '';
           for(let nomeErro in erro.error.errors){
-            const mensagemErro = erro.error.errors[nomeErro];
-            /*this.toastService.show('Erro ao adicionario funcionario: ' + mensagemErro,
-            { classname: 'bg-danger text-light', delay: 5000 });*/
-            console.log('Erro ao adicionario funcionario: ' + mensagemErro)
+            mensagemErro += erro.error.errors[nomeErro];
           }
+          this.toastService.show('Erro ao adicionario funcionario: ' + mensagemErro,
+            { classname: 'bg-danger text-light', delay: 5000 });
         });
   }
 
